0b352c53b1fc8596e89025d848961215f37f446b
Amir <amirhafidz21@gmail.com>
2024-11-20 23:35:51 +0800

    modify ui

---
+++ src/views/contact/contact.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="mt-4">
+        <div class="row mx-0 my-0">
+            <div class="card py-4" style="border-radius: 0; border: none;">
+                <div class="col-12 d-flex justify-content-center mb-3">
+                    <div class="col-11">
+                        <h3 class="mb-3" style="color: #624A44">Contact</h3>
+                        <div class="card py-3 mb-4">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="ms-3">
+                                    <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" class="rounded-circle" style="width: 90px;" alt="Avatar" />
+                                </div>
+                                <div class="me-3 p-1 text-end">
+                                    <h5>Amir Hamzah Bin Haji Kannaung</h5>
+                                    <p>+60 128170137</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card py-3 mb-4">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="ms-3">
+                                    <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" class="rounded-circle" style="width: 90px;" alt="Avatar" />
+                                </div>
+                                <div class="me-3">
+                                    <h4>Amir Hafidz</h4>
+                                    <p>+60 128170137</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card py-3">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="ms-3">
+                                    <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" class="rounded-circle" style="width: 90px;" alt="Avatar" />
+                                </div>
+                                <div class="me-3">
+                                    <h4>Amir Hafidz</h4>
+                                    <p>+60 128170137</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'contact-page',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -2,16 +2,18 @@
     <div>
         <div>
             <HomePage v-if="selected == 1" />
+            <ContactPage v-if="selected == 3" />
             <LocationPage v-if="selected == 2" />
             <CalendarPage v-if="selected == 4" />
         </div>
-        <SwipeBottomNavigation  :options="options" v-model="selected" />
+        <SwipeBottomNavigation swiper-color='#614A43' icon-color='#614A43'  :options="options" v-model="selected" />
     </div>
 </template>
 
 <script>
 import { SwipeBottomNavigation  } from "bottom-navigation-vue";
 import HomePage from '@/views/home/home.vue'
+import ContactPage from '@/views/contact/contact.vue'
 import LocationPage from '@/views/location/location.vue'
 import CalendarPage from '@/views/calendar/calendar.vue'
 import "bottom-navigation-vue/dist/style.css";
@@ -20,6 +22,7 @@ export default {
     name: 'Main-Page',
     components: { 
         HomePage,
+        ContactPage,
         LocationPage,
         CalendarPage,
         SwipeBottomNavigation  
@@ -29,7 +32,7 @@ export default {
             selected: 1,
             options: [
                 { id: 2, icon: 'fa-solid fa-location-dot', title: 'Location', color: '#ac4793' },
-                { id: 3, icon: 'fa-solid fa-heart', title: 'Likes', color: '#e2a93a' },
+                { id: 3, icon: 'fa-solid fa-heart', title: 'Contact', color: '#e2a93a' },
                 { id: 1, icon: 'fa-solid fa-home', title: 'Home', color: '#5639af' },
                 { id: 4, icon: 'fa-solid fa-calendar', title: 'Calendar', color: '#4493a7' },
                 { id: 5, icon: 'fa-solid fa-ring', title: 'About Us', color: '#4493a7' }
